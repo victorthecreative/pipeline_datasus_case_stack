e7fbc56fe715bae77d63cb7410d801c1c2ba05d7
Victor Alves <vitaopoter680@gmail.com>
2023-09-23 05:44:50 +0000

    criação da fato de aplicação de vacina

--- models/marts/fact_aplicacao_vacinas.sql
+++ models/marts/fact_aplicacao_vacinas.sql
@@ -1,16 +1,38 @@
-with
-    fato as (
-        select
-            dim_pacientes.paciente_sk,
-            dim_endereco.municipio_estado_sk,
-            dim_vacina.vacina_sk,
-            stg_APIdatasus_geral.data_aplicacao,
-            stg_APIdatasus_geral.ano_aplicacao,
-            stg_APIdatasus_geral.mes_aplicacao
-        from {{ ref('stg_APIdatasus_geral') }} as stg_APIdatasus_geral
-        join {{ ref('dim_pacientes') }} as dim_pacientes on stg_APIdatasus_geral.paciente_id = dim_pacientes.paciente_id
-        join {{ ref('dim_endereco') }} as dim_endereco on stg_APIdatasus_geral.municipio = dim_endereco.municipio AND stg_APIdatasus_geral.estado = dim_endereco.estado
-        join {{ ref('dim_vacina') }} as dim_vacina on stg_APIdatasus_geral.vacina_nome = dim_vacina.vacina_nome AND stg_APIdatasus_geral.dose = dim_vacina.dose
+WITH
+    pacientes as (
+        SELECT *
+        FROM {{ ref('dim_paciente') }}
+    ),
+    endereco as (
+        SELECT *
+        FROM {{ ref('dim_endereco') }}
+    ),
+    vacina as (
+        SELECT *
+        FROM {{ ref('dim_vacina') }}
+    ),
+    aplicacao as (
+        SELECT *
+        FROM {{ ref('stg_APIdatasus_geral') }}
+    ),
+    joined_tabelas as (
+        SELECT
+            aplicacao.data_aplicacao,
+            aplicacao.ano_aplicacao,
+            aplicacao.mes_aplicacao,
+            pacientes.paciente_sk as fk_paciente,
+            endereco.municipio_estado_sk as fk_endereco,
+            vacina.vacina_sk as fk_vacina
+        FROM aplicacao
+        LEFT JOIN pacientes ON aplicacao.paciente_id = pacientes.paciente_id
+        LEFT JOIN endereco ON aplicacao.municipio = endereco.municipio AND aplicacao.estado = endereco.estado
+        LEFT JOIN vacina ON aplicacao.vacina_nome = vacina.vacina_nome AND aplicacao.dose = vacina.dose
+    ),
+    transformacoes as (
+        SELECT
+            {{ dbt_utils.generate_surrogate_key(['fk_paciente', 'data_aplicacao', 'fk_vacina']) }} as sk_aplicacao,
+            *
+        FROM joined_tabelas
     )
-select *
-from fato
+SELECT *
+FROM transformacoes
